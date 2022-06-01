b707ef4241dcdccb7bcbd1f80151aa25311031e9
basri0 <67202050+basri0@users.noreply.github.com>
2022-06-02 00:34:07 +0300

    Add project files.

---
+++ Entities/Concrete/LessonTopics.cs
@@ -0,0 +1,14 @@
+﻿using Core.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    public class LessonTopics : IEntity
+    {
+        public int Id { get; set; }
+        public int LessonId { get; set; }
+        public string Name { get; set; }
+    }
+}
